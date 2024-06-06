8175b9a1ef363f93ce200e58e79775ab876cb207
JulianoTimmHub <julianohenriquetimm@gmail.com>
2024-06-06 05:14:05 -0300

    adicionando tela de roadmaps

--- src/routes/all_routes.jsx
+++ src/routes/all_routes.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "../pages/login/Login";
 import Home from "../pages/home/Home";
 import Assignature from "../pages/assignature/Assignature"
+import Roadmaps from "../pages/roadmaps/Roadmaps";
 
 export default function AllRoutes(){
     return(
@@ -11,6 +12,7 @@ export default function AllRoutes(){
             <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
             <Route path="/assignature" element={<Assignature/>} />
+            <Route path="/roadmaps" element={<Roadmaps />} />
         </Routes>
     )
 }
