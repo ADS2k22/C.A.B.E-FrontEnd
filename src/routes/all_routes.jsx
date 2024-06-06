23e1bc9f9b503ad1a94a22bdbfd4ce2469d37ef2
JulianoTimmHub <julianohenriquetimm@gmail.com>
2024-06-06 05:14:05 -0300

    adicionando tela de roadmaps

--- src/routes/all_routes.jsx
+++ src/routes/all_routes.jsx
@@ -4,6 +4,7 @@ import Login from "../pages/login/Login";
 import Home from "../pages/home/Home";
 import Assignature from "../pages/assignature/Assignature"
 import Payments from "../pages/payments/Payments";
+import Roadmaps from "../pages/roadmaps/Roadmaps";
 
 export default function AllRoutes(){
     return(
@@ -12,6 +13,7 @@ export default function AllRoutes(){
             <Route path="/home" element={<Home />} />
             <Route path="/assignature" element={<Assignature/>} />
             <Route path="/payments" element={<Payments/>} />
+            <Route path="/roadmaps" element={<Roadmaps />} />
         </Routes>
     )
 }
